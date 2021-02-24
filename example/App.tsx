985fe60a3e1809fd2ef345d0516a1b5ecf0913ca
FreakyCoder <kurayogun@gmail.com>
2021-02-24 17:00:06 +0300

    path fix

--- example/App.tsx
+++ example/App.tsx
@@ -1,31 +1,30 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-// import Text from "@freakycoder/react-native-custom-text";
+import Text from "@freakycoder/react-native-custom-text";
 // import TextWrapper from "./src/TextWrapper";
-import RNText from "./build/dist/RNText";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <RNText h1 bold center numberOfLines={1}>
+        <Text h1 bold center numberOfLines={1}>
           Heading 1
-        </RNText>
-        <RNText h2 center style={{ marginTop: 16 }}>
+        </Text>
+        <Text h2 center style={{ marginTop: 16 }}>
           Heading 2
-        </RNText>
-        <RNText h3 bold center>
+        </Text>
+        <Text h3 bold center>
           Heading 3
-        </RNText>
-        <RNText h4 center>
+        </Text>
+        <Text h4 center>
           Heading 4
-        </RNText>
-        <RNText h5 bold center>
+        </Text>
+        <Text h5 bold center>
           Heading 5
-        </RNText>
-        <RNText h6 center>
+        </Text>
+        <Text h6 center>
           Heading 6
-        </RNText>
+        </Text>
       </View>
     );
   }
